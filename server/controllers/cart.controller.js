f7dbab3f924b7e4e389f6f8c478249f37ab7f1da
Mishal010 <arknight265@gmail.com>
2025-08-26 23:28:35 +0530

    Add To Cart Function implemented

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -25,3 +25,34 @@ export const getCart = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const addToCart = async (req, res) => {
+  try {
+    const { bookId } = req.params;
+    const { quantity = 1 } = req.body;
+
+    const book = await Book.findById(bookId);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+
+    let cart = await Cart.findOne({ user: req.user.id });
+
+    if (!cart) {
+      cart = new Cart({ user: req.user.id, items: [] });
+    }
+
+    const existingItem = cart.items.find(
+      (item) => item.book.toString() === bookId
+    );
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      cart.items.push({ book: bookId, quantity });
+    }
+
+    await cart.save();
+    res.status(201).json(cart);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
